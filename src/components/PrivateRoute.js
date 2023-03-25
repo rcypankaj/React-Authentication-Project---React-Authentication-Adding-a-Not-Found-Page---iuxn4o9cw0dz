09a473e4c6d9b6a53b348d33fdcc1ab35ef1832e
Pankaj <rcypankaj@gmail.com>
2023-03-25 13:51:49 +0530

    freq

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,34 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
+const PrivateRoute = ({ children, ...rest }) => {
+  //   console.log(children);
+  //   console.log(rest);
+  const isAuthenticated = () => {
+    const username = window.localStorage.getItem("username");
+    const password = window.localStorage.getItem("password");
+    const abc = username && password;
+    console.log(abc);
+    return username && password;
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
